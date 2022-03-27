b35d5e9744266023b060016caa1eb6527d3dbd69
Pavel Yudin <hazumida@gmail.com>
2022-03-27 20:10:03 +0400

    0.00001

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=xemTpcbu7ekBFMJYvYmK74w02Yf0GAwHk4XnwgkQ");
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Post post = mapper.readValue(response.getEntity().getContent(), Post.class);
+
+        String url = post.getUrl();
+        request = new HttpGet(url);
+        response = httpClient.execute(request);
+        byte[] bytes = response.getEntity().getContent().readAllBytes();
+
+        try (FileOutputStream fos = new FileOutputStream(url.split("/")[url.split("/").length - 1])){
+            fos.write(bytes, 0, bytes.length );
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+        }
+
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,35 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+
+    private final String date;
+    private final String explanation;
+    private final String hdurl;
+    private final String media_type;
+    private final String service_version;
+    private final String title;
+    private final String url;
+
+    public Post(
+            @JsonProperty("date") String date,
+            @JsonProperty("explanation") String explanation,
+            @JsonProperty("hdurl") String hdurl,
+            @JsonProperty("media_type") String media_type,
+            @JsonProperty("service_version") String service_version,
+            @JsonProperty("title") String title,
+            @JsonProperty("url") String url
+    ) {
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
